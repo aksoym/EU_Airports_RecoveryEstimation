1d1b3fe1b13193049f2a3525d6313ca69f5d6d03
aksoy <aksoym15@itu.edu.tr>
2022-03-24 16:54:13 +0300

    uncertainty sampling tests

--- uncertainty_simulations/uncertainty_matrix_to_discrete.py
+++ uncertainty_simulations/uncertainty_matrix_to_discrete.py
@@ -1,9 +1,6 @@
 import numpy as np
-from typing import List
+from typing import List, Dict
 import pandas as pd
-
-flow_matrix_path = "../recovery_rate/recovery_rate_training_dataframes/data/all_time_flight_flow_df_rev1.pickle"
-flight_flow_data = pd.read_pickle(flow_matrix_path)
 
 class UncertaintyMatrix():
     
@@ -60,17 +57,16 @@ class UncertaintyMatrix():
                     pass
         return array
             
-        
-def find_max_in_nested_array(array: np.ndarray) -> float:
-    max_value = -np.inf
-    for element in array.flatten():
-        if max_value < np.amax(element):
-            max_value = np.amax(element)
-    return max_value
+    @staticmethod
+    def _find_max_in_nested_array(array: np.ndarray) -> float:
+        max_value = -np.inf
+        for element in array.flatten():
+            if max_value < np.amax(element):
+                max_value = np.amax(element)
+        return max_value
 
-def delay_matrix_2_flow_matrices(array: np.ndarray) -> List[np.ndarray]:
-    global_max = find_max_in_nested_array(array)
-    global_min = 0
+def to_flowmatrix(array: np.ndarray) -> List[np.ndarray]:
+    global_max = UncertaintyMatrix._find_max_in_nested_array(array)
     interval = 15 #in minutes.
     num_infection_matrix_windows = np.ceil(global_max / interval).astype(int)
     
@@ -91,63 +87,55 @@ def delay_matrix_2_flow_matrices(array: np.ndarray) -> List[np.ndarray]:
                 
     return infection_matrix_list
 
+def find_max_sublist_length(main_list):
+    max_ = -np.inf
+    for sublist in main_list:
+        if max_ < len(sublist):
+            max_ = len(sublist)
+    return max_
+         
+def flowmatrix_sort(flow_matrix_list: List[np.ndarray]) -> Dict:
+    flow_matrix_dict = dict()
+    for i in range(find_max_sublist_length(flow_matrix_list)):
+        flow_matrices = []
+        for idx, sublist in enumerate(flow_matrix_list):
+            if not flow_matrices:
+                try:
+                    flow_matrices.append(sublist[i])
+                except:
+                    pass
+            if flow_matrices:
+                try:
+                    if not np.array_equal(sublist[i], flow_matrices[idx-1]):
+                        flow_matrices.append(sublist[i])
+                except:
+                    pass
+        flow_matrix_dict[i] = flow_matrices
+    return flow_matrix_dict
+     
+def flowmat_dict_to_infmat_dict(flow_matrix_dict):
+    inf_matrix_dict = dict()
+    for key, array_subarray in flow_matrix_dict.items():
+        new_subarray_list = []
+        for array in array_subarray:
+            result = np.divide(array, array.sum(axis=1, keepdims=True), out=np.zeros_like(array), where=(array > 0), casting="unsafe")
+            new_subarray_list.append(result)
+        inf_matrix_dict[key] = new_subarray_list
+    return inf_matrix_dict
+    
+    
+if __name__ == "__main__":
+    np.set_printoptions(suppress=True, threshold=2e4)
+    flow_matrix_path = "../recovery_rate/recovery_rate_training_dataframes/data/all_time_flight_flow_df_rev1.pickle"
+    flight_flow_data = pd.read_pickle(flow_matrix_path)
 
+    uncertainty_matrix = UncertaintyMatrix(flight_flow_data.loc[("2018-02-08", 48, slice(None)), :].values)
+    arrays = uncertainty_matrix.draw_sample(100)
 
-uncertainty_matrix = UncertaintyMatrix(flight_flow_data.loc[("2018-02-08", 48, slice(None)), :].values)
-arrays = uncertainty_matrix.draw_sample(100)
-
-inf_matrix_list = []
-for array in arrays:
-    inf_matrix_list.append(delay_matrix_2_flow_matrices(array))
+    flow_matrix_nested_list = [to_flowmatrix(array) for array in arrays]
+    organized_flow_matrix_dict = flowmatrix_sort(flow_matrix_nested_list)
+    inf_matrix_dict = flowmat_dict_to_infmat_dict(organized_flow_matrix_dict)
     
-    
-max_ = -np.inf
-min_ = np.inf
-for sublist in inf_matrix_list:
-    if max_ < len(sublist):
-        max_ = len(sublist)
-    if min_ > len(sublist):
-        min_ = len(sublist)
-        
-
-inf_matrix_dict = dict()
-for i in range(max_):
-    inf_matrices = []
-    for idx, sublist in enumerate(inf_matrix_list):
-        if not inf_matrices:
-            try:
-                inf_matrices.append(sublist[i])
-            except:
-                pass
-        if inf_matrices:
-            try:
-                if not np.array_equal(sublist[i], inf_matrices[idx-1]):
-                    inf_matrices.append(sublist[i])
-            except:
-                pass
-    inf_matrix_dict[i] = inf_matrices
-    
-
-print(inf_matrix_dict[12])
-print(len(inf_matrix_dict[12]))
-    
-    
-for idx in range(len(inf_matrix_dict[12])- 1):
-    arrays = inf_matrix_dict[12]
-    is_equal = np.array_equal(arrays[idx], arrays[idx+1])
-    
-    if is_equal:
-        print(is_equal, idx)
-        
-        
-np.set_printoptions(suppress=True, threshold=2e4)
-for array_subarray in inf_matrix_dict.values():
-    for array in array_subarray:
-        result = np.divide(array, array.sum(axis=1, keepdims=True), where=(array != 0), casting="unsafe")
-        print(result)
-        print(array)
-        break
-    break
         
     
                 
